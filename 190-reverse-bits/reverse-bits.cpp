86245ce75ab4cb0c0fff97f61c1b51a4016e9c49
chaudhary7307 <sanjanac737@gmail.com>
2026-02-16 06:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.07%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,11 +3,10 @@ public:
     int reverseBits(int n) {
         long long ans=0;
         for(int i=31;i>=0;i--){
-            if(n==0)
-            break;
+            if(n==0) break;
             else if(n%2==1)
             ans += pow(2,i);
-            n /= 2;
+            n /=2;
 
         }
         return ans;
